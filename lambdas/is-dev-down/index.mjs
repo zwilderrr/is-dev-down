d88ee60630d27afee934171d2d6aea4f3715863b
zack wilder <zwilder@gmail.com>
2024-07-30 12:18:53 -0400

    add lambdas

---
+++ lambdas/is-dev-down/index.mjs
@@ -0,0 +1,31 @@
+import AWS from "aws-sdk";
+const { DynamoDB } = AWS;
+
+const dynamoDB = new DynamoDB.DocumentClient();
+
+export const handler = async event => {
+  const params = {
+    TableName: "VisitCount",
+    Key: { id: "visitCount" },
+    UpdateExpression: "SET #count = #count + :inc",
+    ExpressionAttributeNames: { "#count": "count" },
+    ExpressionAttributeValues: { ":inc": 1 },
+    ReturnValues: "UPDATED_NEW",
+  };
+
+  try {
+    const result = await dynamoDB.update(params).promise();
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(result.Attributes.count),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+};
